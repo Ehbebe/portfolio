8718f1866f805d4d5d442966c41965c73dc5a8c6
Ehbebe <study2hojin@gmail.com>
2024-04-19 16:50:14 +0900

    Add ALLOWED_HOSTS

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-jc2%)eme#p0k*m8kuk0e%d+c*ox+=y#44wg5h6pd@@m)4c6=)&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
